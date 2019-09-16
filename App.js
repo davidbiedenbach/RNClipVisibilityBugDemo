29c683a6adf5bc3954bb4ed1c1e138b94dd21d76
David Biedenbach <david.biedenbach@sony.com>
2019-09-16 14:50:59 -0700

    modes for each broken case, point RN dep. to facebook#master

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
   Text,
   View,
   ScrollView,
+  FlatList,
   Button,
   NativeModules
 } from 'react-native';
@@ -11,7 +12,8 @@ import {
 const {CustomVisibilityInspector} = NativeModules;
 
 const ITEMS_COUNT = 100;
-const INITIAL_TEXT = 'The list of natively visible items will appear here once you tap on the button. According to the bug, the items hidden by this panel (drawn below it, z-axis-wise) will show as well (though they should not).';
+const INITIAL_TEXT = 'The list of natively visible items will appear here once you tap on the button. With the bug in effect, items that are fully clipped by their parent hierarchy will show as visible even though they should not.';
+const Modes = { View: "View", ScrollView : "ScrollView", HorizontalScrollView: "HorizontalScrollView", FlatList: "FlatList" };
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -19,7 +21,8 @@ export default class App extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      visibilityDesc: ''
+      visibilityDesc: '',
+      mode: Modes.View
     };
 
     this.onDescribeVisibility = this.onDescribeVisibility.bind(this);
@@ -32,18 +35,60 @@ export default class App extends Component<Props> {
   }
 
   render() {
+    const { mode } = this.state;
     return (
       <View style={styles.container}>
-        <View style={{backgroundColor: 'lightgray', height: "75%", overflow: 'hidden'}} nativeID="visibilityInspect">
-          <ScrollView>
-            <Text style={styles.itemText}>This list is scrollable</Text>
-            {
-              Array.from(
-                {length: ITEMS_COUNT},
-                (value, index) => <Text key={`item-${index}`} style={styles.itemText}>[Item #{index}]</Text>
-              )
-            }
-          </ScrollView>
+        <View style={{backgroundColor: 'lightgray', height: "75%", width: "75%", overflow: 'hidden'}} nativeID="visibilityInspect">
+          {
+            (() => {
+              switch (mode) {
+                case Modes.View:
+                  return (
+                    <View>
+                    <Text style={styles.itemText}>This list is not scrollable</Text>
+                    {
+                      Array.from(
+                        {length: ITEMS_COUNT},
+                        (value, index) => <Text key={`item-${index}`} style={styles.itemText}>[Item #{index}]</Text>
+                      )
+                    }
+                  </View>        
+                  );
+                case Modes.ScrollView:
+                  return (
+                    <ScrollView>
+                    <Text style={styles.itemText}>This list is scrollable</Text>
+                    {
+                      Array.from(
+                        {length: ITEMS_COUNT},
+                        (value, index) => <Text key={`item-${index}`} style={styles.itemText}>[Item #{index}]</Text>
+                      )
+                    }
+                    </ScrollView>        
+                  );
+                case Modes.HorizontalScrollView:
+                  return (
+                    <ScrollView horizontal={true} style={{backgroundColor:"#cfcfcf", overflow: 'hidden'}}>
+                    <Text style={styles.itemText}>This list is scrollable</Text>
+                    {
+                      Array.from(
+                        {length: ITEMS_COUNT},
+                        (value, index) => <Text key={`item-${index}`} style={styles.itemText}>[Item #{index}]</Text>
+                      )
+                    }
+                    </ScrollView>        
+                  );
+                case Modes.FlatList:
+                  return (
+                    <FlatList
+                      data={Array.from({ length: ITEMS_COUNT }, (value, index) => index)}
+                      renderItem={({ index }) => <Text key={`item-${index}`} style={styles.itemText}>[Item #{index}]</Text>}
+                      keyExtractor={(item, index) => index.toString()}
+                    />
+                  );        
+              }
+            })()
+          }
         </View>
 
         <View style={{backgroundColor: 'lightskyblue', flex: 1, paddingHorizontal: 8}}>
@@ -54,6 +99,36 @@ export default class App extends Component<Props> {
           {!this.state.visibilityDesc ? <Text style={styles.freeText}>{INITIAL_TEXT}</Text> : undefined}
           {this.state.visibilityDesc ? <Text style={{...styles.freeText, marginBottom: 5}}>Android says these items are visible:</Text> : undefined}
           <Text style={styles.freeText}>{this.state.visibilityDesc}</Text>
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Button
+            type="primary"
+            title="View"
+            onPress={() => {
+              this.setState({ mode: Modes.View });
+            }}
+          />
+          <Button
+            type="primary"
+            title="ScrollView"
+            onPress={() => {
+              this.setState({ mode: Modes.ScrollView });
+            }}
+          />
+          <Button
+            type="primary"
+            title="FlatList"
+            onPress={() => {
+              this.setState({ mode: Modes.FlatList });
+            }}
+          />
+          <Button
+            type="primary"
+            title="HorizontalScrollView"
+            onPress={() => {
+              this.setState({ mode: Modes.HorizontalScrollView });
+            }}
+          />
         </View>
       </View>
     );
